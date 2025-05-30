e513904d04a197f67c01ea1024fb057523631a37
Paul <Paul@gmail.com>
2025-05-30 22:21:10 +0300

    four commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,44 @@
-let name = "Ivan";
-let number = 7;
-const pi = 3.14;
+$(document).ready(function () {
+    $('.carousel__inner').slick({
+        infinite: true,
+        speed: 1200,
+        slidesToShow: 1,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/slider/chevron-left-solid.png"></button',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/slider/chevron-right-solid.png"></button>',
+        responsive:
+            [
+                {
+                    breakpoint: 972,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        dots: true,
+                        arrows: false
+                    }
+                }
+            ]
+    });
+    $('.slick-dots__button').text('');
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+        $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
 
-let leftBorderWidth = 200;
 
+
+    function toggleSlide(item) {
+        $(item).each(function (i) {
+            $(this).on('click', function (e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            });
+        });
+    };
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+});
 
